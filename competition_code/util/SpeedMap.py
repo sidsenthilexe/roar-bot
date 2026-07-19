87ab3b7eac7d9f2960be615379b354b19fc9866c
Sid Senthil <sidsenthil2010@gmail.com>
2026-07-19 09:48:17 -0700

    less again and edit center

--- competition_code/util/SpeedMap.py
+++ competition_code/util/SpeedMap.py
@@ -2,12 +2,12 @@ class SpeedMap:
     
     @staticmethod
     def get_target(curvature):
-        if (curvature < 0.00025):
+        if (curvature < 0.00015):
             return 75
         elif (curvature < 0.0005):
             return 65
         elif (curvature < 0.0025):
-            return 55
+            return 53
         elif (curvature < 0.006):
             return 45
         elif (curvature < 0.01):
