d251b36b52e85c53a5736ef1e471c716673209e6
Sid Senthil <sidsenthil2010@gmail.com>
2026-07-19 07:24:32 -0700

    revert map

--- competition_code/util/SpeedMap.py
+++ competition_code/util/SpeedMap.py
@@ -5,15 +5,11 @@ class SpeedMap:
         if (curvature < 0.0005):
             return 60
         elif (curvature < 0.0025):
-            return 58
-        elif (curvature < 0.0055):
+            return 55
+        elif (curvature < 0.006):
             return 45
-        elif (curvature < 0.0075):
-            return 40
-        elif (curvature < 0.0105):
+        elif (curvature < 0.01):
             return 30
-        elif (curvature < 0.015):
-            return 22
         elif (curvature < 0.02):
             return 20
         else:
