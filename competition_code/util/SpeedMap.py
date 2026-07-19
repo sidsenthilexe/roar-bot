75fff486d172beb68ae83249f3088c1d8391909e
Sid Senthil <sidsenthil2010@gmail.com>
2026-07-19 16:51:26 -0700

    lookahead threshold

--- competition_code/util/SpeedMap.py
+++ competition_code/util/SpeedMap.py
@@ -7,9 +7,9 @@ class SpeedMap:
         if (curvature < 0.0003):
             return 85
         elif (curvature < 0.0005):
-            return 65
+            return 75
         elif (curvature < 0.0025):
-            return 53
+            return 50
         elif (curvature < 0.006):
             return 45
         elif (curvature < 0.01):
