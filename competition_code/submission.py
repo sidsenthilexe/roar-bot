7c0c6915cae1f54afa8801b2a4a48b73b7a8513e
Sid Senthil <sidsenthil2010@gmail.com>
2026-07-19 06:58:38 -0700

    print target speed and revert

--- competition_code/submission.py
+++ competition_code/submission.py
@@ -93,9 +93,9 @@ class RoarCompetitionSolution:
 
 
         
-        speed_wp_1 = self.maneuverable_waypoints[(self.current_waypoint_idx + int(vehicle_velocity_norm)) % len(self.maneuverable_waypoints)]
+        speed_wp_1 = self.maneuverable_waypoints[(self.current_waypoint_idx + 20) % len(self.maneuverable_waypoints)]
 
-        speed_wp_2 = self.maneuverable_waypoints[(self.current_waypoint_idx + int(vehicle_velocity_norm)+20) % len(self.maneuverable_waypoints)]
+        speed_wp_2 = self.maneuverable_waypoints[(self.current_waypoint_idx + 40) % len(self.maneuverable_waypoints)]
 
         vector_wp_1 = WaypointCalculator.vector_to_waypoint(speed_wp_1, current_waypoint)
         
@@ -140,6 +140,6 @@ class RoarCompetitionSolution:
             "reverse": 0,
             "target_gear": 0
         }
-        print(f"Current waypoint idx: {self.current_waypoint_idx}, Curvature: {curvature}")
+        print(f"Current waypoint idx: {self.current_waypoint_idx}, Curvature: {curvature}, Target Speed: {target_speed}, Current Speed: {vehicle_velocity_norm}")
         await self.vehicle.apply_action(control)
         return control
