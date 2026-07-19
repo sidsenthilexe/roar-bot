0cd8c83a9e2a4e2012b15c8b308e41ae1377af5e
Sid Senthil <sidsenthil2010@gmail.com>
2026-07-19 07:51:01 -0700

    revert test

--- competition_code/tmp.py
+++ competition_code/tmp.py
@@ -6,6 +6,9 @@ Please do not change anything else but fill out the to-do sections.
 from typing import List, Tuple, Dict, Optional
 import roar_py_interface
 import numpy as np
+from util.SpeedMap import SpeedMap
+from util.SteerMap import SteerMap
+from util.WaypointCalculator import WaypointCalculator
 
 def normalize_rad(rad : float):
     return (rad + np.pi) % (2 * np.pi) - np.pi
@@ -82,42 +85,36 @@ class RoarCompetitionSolution:
         )
 
         #
-        steering_ahead = 18
-        if (vehicle_velocity_norm < 20):
-            steering_ahead = 6
-        elif (vehicle_velocity_norm < 35):
-            steering_ahead = 12
-        elif (vehicle_velocity_norm < 50):
-            steering_ahead = 18
-        else:
-            steering_ahead = 25
+        look_ahead = SteerMap.look_ahead_dist(vehicle_velocity_norm)
 
          # We use the 3rd waypoint ahead of the current waypoint as the target waypoint
-        waypoint_to_follow = self.maneuverable_waypoints[(self.current_waypoint_idx + steering_ahead) % len(self.maneuverable_waypoints)]
+        current_waypoint = self.maneuverable_waypoints[self.current_waypoint_idx]
+        target_waypoint = self.maneuverable_waypoints[(self.current_waypoint_idx + look_ahead) % len(self.maneuverable_waypoints)]
 
-        # 
-        speed_waypoint_1 = self.maneuverable_waypoints[(self.current_waypoint_idx + 20) % len(self.maneuverable_waypoints)]
 
-        speed_waypoint_2 = self.maneuverable_waypoints[(self.current_waypoint_idx + 40) % len(self.maneuverable_waypoints)]
-
-        vector_to_speed_waypoint_1 = (speed_waypoint_1.location - self.maneuverable_waypoints[self.current_waypoint_idx].location)[:2]
         
-        vector_to_speed_waypoint_2 = (speed_waypoint_2.location - speed_waypoint_1.location)[:2]
+        speed_wp_1 = self.maneuverable_waypoints[(self.current_waypoint_idx + 20) % len(self.maneuverable_waypoints)]
 
-        heading_to_speed_waypoint_1 = np.arctan2(vector_to_speed_waypoint_1[1],vector_to_speed_waypoint_1[0])
+        speed_wp_2 = self.maneuverable_waypoints[(self.current_waypoint_idx + 40) % len(self.maneuverable_waypoints)]
 
-        heading_to_speed_waypoint_2 = np.arctan2(vector_to_speed_waypoint_2[1],vector_to_speed_waypoint_2[0])
+        vector_wp_1 = WaypointCalculator.vector_to_waypoint(speed_wp_1, current_waypoint)
+        
+        vector_wp_2 = WaypointCalculator.vector_to_waypoint(speed_wp_2, current_waypoint)
 
-        distance_car_waypoint_to_speed_waypoint_1 = np.linalg.norm((speed_waypoint_1.location - self.maneuverable_waypoints[self.current_waypoint_idx].location)[:2])
+        hdg_wp_1 = WaypointCalculator.hdg(vector_wp_1)
 
-        distance_speed_waypoint_1_to_speed_waypoint_2 = np.linalg.norm((speed_waypoint_2.location - speed_waypoint_1.location)[:2])
+        hdg_wp_2 = WaypointCalculator.hdg(vector_wp_2)
 
-        heading_diff = normalize_rad(heading_to_speed_waypoint_2 - heading_to_speed_waypoint_1)
+        first_element_dist = WaypointCalculator.dist(speed_wp_1, current_waypoint)
 
-        curvature = abs(heading_diff) / (distance_car_waypoint_to_speed_waypoint_1 + distance_speed_waypoint_1_to_speed_waypoint_2)
+        second_element_dist = WaypointCalculator.dist(speed_wp_1, speed_wp_2)
+
+        heading_diff = normalize_rad(hdg_wp_2 - hdg_wp_1)
+
+        curvature = abs(heading_diff) / (first_element_dist + second_element_dist)
 
         # Calculate delta vector towards the target waypoint
-        vector_to_waypoint = (waypoint_to_follow.location - vehicle_location)[:2]
+        vector_to_waypoint = (target_waypoint.location - vehicle_location)[:2]
         heading_to_waypoint = np.arctan2(vector_to_waypoint[1],vector_to_waypoint[0])
 
         # Calculate delta angle towards the target waypoint
@@ -129,24 +126,11 @@ class RoarCompetitionSolution:
         ) if vehicle_velocity_norm > 1e-2 else -np.sign(delta_heading)
         steer_control = np.clip(steer_control, -1.0, 1.0)
 
-        #
-        target_speed = 15
-        if (curvature < 0.0005):
-            target_speed = 60
-        elif (curvature < 0.0025):
-            target_speed = 55
-        elif (curvature < 0.006):
-            target_speed = 45
-        elif (curvature < 0.01):
-            target_speed = 30
-        elif (curvature < 0.02):
-            target_speed = 20
-        else:
-            target_speed = 10
+        target_speed = SpeedMap.get_target(curvature)
 
 
         # Proportional controller to control the vehicle's speed towards 40 m/s
-        throttle_control = 0.1 * (target_speed - vehicle_velocity_norm)
+        throttle_control = 0.15 * (target_speed - vehicle_velocity_norm)
 
         control = {
             "throttle": np.clip(throttle_control, 0.0, 1.0),
@@ -156,6 +140,6 @@ class RoarCompetitionSolution:
             "reverse": 0,
             "target_gear": 0
         }
-        print(f"Current waypoint idx: {self.current_waypoint_idx}, Curvature: {curvature}")
+        print(f"Current waypoint idx: {self.current_waypoint_idx}, Curvature: {curvature}, Target Speed: {target_speed}, Current Speed: {vehicle_velocity_norm}, Throttle%: {throttle_control}")
         await self.vehicle.apply_action(control)
         return control
