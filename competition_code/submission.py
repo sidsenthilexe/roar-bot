61a457954994e9f7a48d835fcc0495698766de07
ItsGetWreckt <samanyu.rayala@gmail.com>
2026-07-19 09:42:37 -0700

    test

--- competition_code/submission.py
+++ competition_code/submission.py
@@ -48,6 +48,8 @@ class RoarCompetitionSolution:
         # TODO: You can do some initial computation here if you want to.
         # For example, you can compute the path to the first waypoint.
 
+        #test
+
         # Receive location, rotation and velocity data 
         vehicle_location = self.location_sensor.get_last_gym_observation()
         vehicle_rotation = self.rpy_sensor.get_last_gym_observation()
