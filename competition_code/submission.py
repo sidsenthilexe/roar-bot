92b0d133341d471cb6b31bf1331bac62b4e50161
Sid Senthil <sidsenthil2010@gmail.com>
2026-07-19 22:03:39 -0700

    accelerate earlier iii

--- competition_code/submission.py
+++ competition_code/submission.py
@@ -95,7 +95,7 @@ class RoarCompetitionSolution:
         current_waypoint = self.maneuverable_waypoints[self.current_waypoint_idx]
         target_waypoint = self.maneuverable_waypoints[(self.current_waypoint_idx + look_ahead) % len(self.maneuverable_waypoints)]
 
-        spd_look_ahead = np.clip(int(vehicle_velocity_norm), 30, 53)
+        spd_look_ahead = np.clip(int(vehicle_velocity_norm), 33, 53)
         
         speed_wp = [self.maneuverable_waypoints[(self.current_waypoint_idx + spd_look_ahead) % len(self.maneuverable_waypoints)], self.maneuverable_waypoints[(self.current_waypoint_idx + spd_look_ahead+20) % len(self.maneuverable_waypoints)]]
 
