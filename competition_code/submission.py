af89454b306d54d533a58c6c7daec271a483e4a3
Sid Senthil <sidsenthil2010@gmail.com>
2026-07-20 16:27:36 -0700

    revert to wp primary

--- competition_code/submission.py
+++ competition_code/submission.py
@@ -53,7 +53,7 @@ class RoarCompetitionSolution:
 
         # Receive location, rotation and velocity data 
 
-        self.maneuverable_waypoints = roar_py_interface.RoarPyWaypoint.load_waypoint_list(np.load("waypoints/oldWaypointsPrimary4.npz"))
+        self.maneuverable_waypoints = roar_py_interface.RoarPyWaypoint.load_waypoint_list(np.load("waypoints/waypointsPrimary.npz"))
 
 
         vehicle_location = self.location_sensor.get_last_gym_observation()
