6bb501aa7f0f9344c08e3556ee5f7bb85533e770
ItsGetWreckt <samanyu.rayala@gmail.com>
2026-07-21 12:52:13 -0700

    test some stuff out

--- competition_code/util/SteerController.py
+++ competition_code/util/SteerController.py
@@ -4,14 +4,19 @@ from util.MathUtil import MathUtil
 class SteerController:
 
     @staticmethod
-    def get_target_angle(target, car_loc, car_rot, wheelbase):
+    def get_target_angle(target, car_loc, car_rot, vehicle):
+        wheelbase = SteerController.get_vehicle_wheelbase(vehicle)
         dx = target.location[0] - car_loc[0]
         dy = target.location[1] - car_loc[1]
         actual_look_ahead = max(1e-6, np.hypot(dx, dy))
         angle_diff = MathUtil.normalize_rad(np.arctan2(dy, dx) - car_rot[2])
         curvature = 2 * np.sin(angle_diff) / actual_look_ahead
-        curvature_clip = np.clip(curvature, -0.25, 0.25)
-        steer_angle = np.arctan(wheelbase * curvature_clip)
+        steer_angle = np.arctan(wheelbase * curvature)
         return -steer_angle
 
-
+    @staticmethod
+    def get_vehicle_wheelbase(vehicle):
+        wheels = vehicle.get_physics_control().wheels
+        front = (wheels[0].position.x + wheels[1].position.x) / 200
+        back = (wheels[2].position.x + wheels[3].position.x) / 200
+        return abs(front - back)
