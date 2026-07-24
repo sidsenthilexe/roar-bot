0a5cbb538bac6c60eae23269d4e6078b5e4ef3c7
Sid Senthil <sidsenthil2010@gmail.com>
2026-07-24 15:15:22 -0700

    return to step function

--- competition_code/util/SteerMap.py
+++ competition_code/util/SteerMap.py
@@ -1,10 +1,7 @@
-import numpy as np
 class SteerMap:
 
     @staticmethod
     def look_ahead_dist(velocity):
-        return int(np.clip(0.6 * velocity - 3.0, 6.0, 25.0))
-
         if (velocity < 20):
             return 6
         elif (velocity < 35):
