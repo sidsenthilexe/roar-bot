92a7a9518bc3a9e45379aee841409acaa9114e7e
Sid Senthil <sidsenthil2010@gmail.com>
2026-07-24 15:44:21 -0700

    gemini function

--- competition_code/util/SteerMap.py
+++ competition_code/util/SteerMap.py
@@ -1,7 +1,11 @@
+import numpy as np
 class SteerMap:
 
     @staticmethod
     def look_ahead_dist(velocity):
+        return int(np.clip(0.6 * velocity - 3.0, 6.0, 25.0))
+
+
         if (velocity < 20):
             return 6
         elif (velocity < 35):
