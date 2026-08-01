c09b1073c05acb0cab9d0f40591424d9f458c073
Sid Senthil <sidsenthil2010@gmail.com>
2026-07-31 19:40:41 -0700

    increase pid first

--- competition_code/util/Tuner.py
+++ competition_code/util/Tuner.py
@@ -8,9 +8,8 @@ class Tuner:
         return target_speed * 44/35
 
     def decide_steer_pid(vel):
-        if (vel < 25): return 1.2
-        if (vel < 35): return 1.2
-        return 1.3
+        if (vel < 35): return 1.25
+        return 1.35
     
     
     
