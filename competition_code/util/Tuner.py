ecf5d177d91f7b69f4b8d76c89ac44583637b725
Sid Senthil <sidsenthil2010@gmail.com>
2026-07-31 19:22:50 -0700

    random theory pid test

--- competition_code/util/Tuner.py
+++ competition_code/util/Tuner.py
@@ -8,8 +8,8 @@ class Tuner:
         return target_speed * 44/35
 
     def decide_steer_pid(vel):
-        if (vel < 35): return 0.9
-        return 1.0
+        if (vel < 35): return 1.0
+        return 1.1
     
     
     
