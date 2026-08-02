d0dff49c9d73e16182895b01dc4901d2af881a4d
ItsGetWreckt <samanyu.rayala@gmail.com>
2026-08-01 19:11:11 -0700

    334.6

--- competition_code/util/Tuner.py
+++ competition_code/util/Tuner.py
@@ -5,13 +5,18 @@ class Tuner:
         if (waypoint < 450 or 745 <= waypoint <= 870 or (waypoint >= 2550 and target_speed > 35)): return target_speed * 38/35
         elif (1200 <= waypoint <= 1450): return target_speed * 40/35
         elif (waypoint >= 2550): return target_speed 
-        elif (450 <= waypoint <= 510): return min(target_speed, 34.9)
+        elif (450 <= waypoint <= 510): return min(target_speed, 35.1)
         return target_speed * 44/35
 
     @staticmethod
     def tune_steer_kP(target_speed):
         if (target_speed < 35): return 1.2
         return 1.3
+
+    @staticmethod
+    def tune_steer(steer, waypoint):
+        if (475 <= waypoint <= 485): return steer * 2.0
+        return steer
     
     
     
--- competition_code/waypoints/plot_waypoints.py
+++ competition_code/waypoints/plot_waypoints.py
@@ -769,7 +769,7 @@ class TrackEditor:
 
 if __name__ == "__main__":
     REFERENCE_TRACK = "competition_code/waypoints/Monza.npz"
-    EDITABLE_PATH_1 = "competition_code/waypoints/output_theWaypoints.npz"
+    EDITABLE_PATH_1 = "competition_code/waypoints/waypointsPrimary.npz"
     EDITABLE_PATH_2 = "competition_code/waypoints/output_theWaypoints.npz" 
     
     if os.path.exists(REFERENCE_TRACK) and os.path.exists(EDITABLE_PATH_1) and os.path.exists(EDITABLE_PATH_2):
