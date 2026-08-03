81b531f519d09677802128bb536fa5e808b09e56
Sid Senthil <sidsenthil2010@gmail.com>
2026-08-02 19:30:02 -0700

    back to 40 i guess

--- competition_code/util/Tuner.py
+++ competition_code/util/Tuner.py
@@ -3,7 +3,7 @@ class Tuner:
     @staticmethod
     def tune_target_speed(target_speed, waypoint):
         if (waypoint < 450 or 745 <= waypoint <= 870 or (waypoint >= 2550 and target_speed > 35)): return target_speed * 38/35
-        elif (1200 <= waypoint <= 1450): return target_speed * 39/35
+        elif (1200 <= waypoint <= 1450): return target_speed * 40/35
         elif (waypoint >= 2550): return target_speed 
         elif (450 <= waypoint <= 510): return min(target_speed, 35.1)
         return target_speed * 44/35
