b1845681ad6243e9ef6b8bda41b61c128129645d
Sid Senthil <sidsenthil2010@gmail.com>
2026-08-03 20:00:00 -0700

    always pos

--- competition_code/util/Tuner.py
+++ competition_code/util/Tuner.py
@@ -14,7 +14,7 @@ class Tuner:
         return 1.3
 
     def tune_steer(steer, wp):
-        if (745 <= wp <= 870): return steer*2
+        if (745 <= wp <= 870): return abs(steer*2)
         else: return steer
 
     @staticmethod
