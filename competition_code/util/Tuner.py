6ab8cb3aaa0d2e8341dfa9b6b11a7816445d72d4
ItsGetWreckt <samanyu.rayala@gmail.com>
2026-08-04 11:07:00 -0700

    328.15

--- competition_code/util/Tuner.py
+++ competition_code/util/Tuner.py
@@ -4,7 +4,8 @@ class Tuner:
 
     @staticmethod
     def tune_target_speed(target_speed, waypoint):
-        if (745 <= waypoint <= 870): return target_speed * 40/35
+        if (1815 <= waypoint <= 2000): return target_speed * 10/7
+        if (745 <= waypoint <= 870): return target_speed * 8/7
         if (waypoint <= 510 or waypoint >= 2550): return target_speed * 38/35
         return target_speed * 44/35
 
@@ -20,8 +21,8 @@ class Tuner:
 
     @staticmethod
     def tune_steer_lookahead(lookahead, waypoint):
-        if (745 <= waypoint < 838): return lookahead + 4
         if (1300 <= waypoint <= 1325 or 1400 <= waypoint <= 1425): return lookahead + 7
-        elif (450 <= waypoint < 490 or 2550 <= waypoint <= 2600): return lookahead + 2
+        if (1800 <= waypoint <= 1900 or 745 <= waypoint < 838): return lookahead + 4
+        if (450 <= waypoint < 490 or 2550 <= waypoint <= 2600): return lookahead + 2
         return lookahead  
     
