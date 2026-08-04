a930f4173c7e9a8b188ed589dfe03470ded54c10
Sid Senthil <sidsenthil2010@gmail.com>
2026-08-04 16:30:23 -0700

    revert to 1300

--- competition_code/util/Tuner.py
+++ competition_code/util/Tuner.py
@@ -4,7 +4,7 @@ class Tuner:
 
     @staticmethod
     def tune_target_speed(target_speed, waypoint):
-        if (900 <= waypoint <= 1325 or 1400 <= waypoint <= 1425): return target_speed * 10/7
+        if (1300 <= waypoint <= 1325 or 1400 <= waypoint <= 1425): return target_speed * 10/7
         if (1800 <= waypoint <= 1900): return target_speed * 12/7
         if (1900 <= waypoint <= 2000): return target_speed * 10/7
         if (745 <= waypoint <= 870): return target_speed * 8/7
