efeecdad39e50c264a666022c0a5b0caf911c0e8
Sid Senthil <sidsenthil2010@gmail.com>
2026-08-04 16:29:07 -0700

    i guess bro

--- competition_code/util/Tuner.py
+++ competition_code/util/Tuner.py
@@ -5,7 +5,8 @@ class Tuner:
     @staticmethod
     def tune_target_speed(target_speed, waypoint):
         if (900 <= waypoint <= 1325 or 1400 <= waypoint <= 1425): return target_speed * 10/7
-        if (1815 <= waypoint <= 2000): return target_speed * 10/7
+        if (1800 <= waypoint <= 1900): return target_speed * 12/7
+        if (1900 <= waypoint <= 2000): return target_speed * 10/7
         if (745 <= waypoint <= 870): return target_speed * 8/7
         if (waypoint <= 510 or waypoint >= 2550): return target_speed * 38/35
         return target_speed * 44/35
