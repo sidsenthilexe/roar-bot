412107604292e8aded6b5ed7fb4690f47ebde534
ItsGetWreckt <samanyu.rayala@gmail.com>
2026-08-06 14:01:19 -0700

    325.9/326.2

--- competition_code/util/Tuner.py
+++ competition_code/util/Tuner.py
@@ -6,8 +6,8 @@ class Tuner:
     def tune_target_speed(target_speed, waypoint):
         if (0 <= waypoint <= 300): return 80
         if (1300 <= waypoint <= 1325 or 1400 <= waypoint <= 1425): return target_speed * 10/7
-        if (1750 <= waypoint <= 2000): return max(target_speed * 10.5/7, 45)
-        if (745 <= waypoint <= 870): return target_speed * 8/7
+        if (1750 <= waypoint <= 2000): return max(target_speed * 3/2, 45)
+        if (745 <= waypoint <= 870): return target_speed * 6/5
         if (waypoint <= 510 or waypoint >= 2550): return target_speed * 38/35
         return target_speed * 44/35
 
@@ -18,16 +18,15 @@ class Tuner:
 
     @staticmethod
     def tune_steer(steer, waypoint):
-        if (840 <= waypoint <= 860): return max(steer * 2.0, 0)
+        if (830 <= waypoint <= 860): return max(steer * 2.0, 0)
         return steer
 
     @staticmethod
     def tune_steer_lookahead(lookahead, waypoint):
-        if (1300 <= waypoint <= 1325 or 1400 <= waypoint <= 1425): return lookahead + 7
-        if (1800 <= waypoint <= 1900): return lookahead + 7
+        if (1300 <= waypoint <= 1325 or 1400 <= waypoint <= 1425 or 1800 <= waypoint <= 1900): return lookahead + 7
         if (745 <= waypoint < 838): return lookahead + 4
         if (450 <= waypoint < 490 or 2550 <= waypoint <= 2600): return lookahead + 2
-        return lookahead  
+        return lookahead
 
     @staticmethod
     def tune_brake(brake, waypoint):
