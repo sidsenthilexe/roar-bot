23a56a2f6f196cc933f2db31b8c5682cefc9b198
Sid Senthil <sidsenthil2010@gmail.com>
2026-08-05 19:33:55 -0700

    less range

--- competition_code/util/Tuner.py
+++ competition_code/util/Tuner.py
@@ -4,7 +4,7 @@ class Tuner:
 
     @staticmethod
     def tune_target_speed(target_speed, waypoint):
-        if (0 <= 400): return 80
+        if (0 <= 300): return 80
         if (1300 <= waypoint <= 1325 or 1400 <= waypoint <= 1425): return target_speed * 10/7
         if (1750 <= waypoint <= 2000): return max(target_speed * 11/7, 45)
         if (745 <= waypoint <= 870): return target_speed * 8/7
