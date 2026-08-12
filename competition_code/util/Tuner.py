9947efec2379fb24e52754410f881c8f6f015f8a
Sid Senthil <sidsenthil2010@gmail.com>
2026-08-11 21:02:12 -0700

    damp instead early part

--- competition_code/util/Tuner.py
+++ competition_code/util/Tuner.py
@@ -32,7 +32,7 @@ class Tuner:
 
     @staticmethod
     def tune_steer(steer, waypoint):
-        if (490 <= waypoint < 505): return steer * 3.0
+        if (490 <= waypoint < 505): return steer * 0.7
         if (510 <= waypoint <= 529): return max(steer * 3.0, 0)
         if (1855 <= waypoint <= 1900): return max(steer * 3.0, 0)
         if (830 <= waypoint < 860): return max(steer * 1.5, 0)
