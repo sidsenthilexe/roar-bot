4b6e76384ee697b10b95154c937c60b59f6d1ea9
Sid Senthil <sidsenthil2010@gmail.com>
2026-08-11 19:28:38 -0700

    reduce on exit l2

--- competition_code/util/Tuner.py
+++ competition_code/util/Tuner.py
@@ -33,7 +33,7 @@ class Tuner:
     @staticmethod
     def tune_steer(steer, waypoint):
         if (1855 <= waypoint <= 1900): return max(steer * 3.0, 0)
-        if (830 <= waypoint < 870): return max(steer * 1.5, 0)
+        if (830 <= waypoint < 865): return max(steer * 1.5, 0)
         return steer
 
     @staticmethod
