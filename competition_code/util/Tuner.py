053e8c149f3e6a3cbd24c0daf3ba7f64698fbb98
Sid Senthil <sidsenthil2010@gmail.com>
2026-08-11 20:58:48 -0700

    reduce the early one

--- competition_code/util/Tuner.py
+++ competition_code/util/Tuner.py
@@ -32,7 +32,7 @@ class Tuner:
 
     @staticmethod
     def tune_steer(steer, waypoint):
-        if (460 <= waypoint < 505): return steer * 2.0
+        if (480 <= waypoint < 505): return steer * 1.5
         if (510 <= waypoint <= 529): return max(steer * 3.0, 0)
         if (1855 <= waypoint <= 1900): return max(steer * 3.0, 0)
         if (830 <= waypoint < 860): return max(steer * 1.5, 0)
